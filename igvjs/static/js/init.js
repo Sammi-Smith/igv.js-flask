6a84a8453c125b3cfefcb67ca0a44fe81312bb46
Sammi Smith <sammi.smith@peraton.com>
2021-12-01 17:04:16 -0500

    initial view with reference genome and prefilter alignments

--- igvjs/static/js/init.js
+++ igvjs/static/js/init.js
@@ -6,18 +6,28 @@ function initBrowser() {
 
   div = document.getElementById("myDiv");
   options = {
-      genome: "hg19",
-      //locus: "22:24,375,771-24,376,878",
+      reference: {            
+                      id: 'id',
+                      name: 'name',
+                      //fastaURL: 'https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg19/hg19.fasta',
+                      //fastaURL must include http://localhost:5000 to view a local file instead of URL
+                      fastaURL: 'http://localhost:5000/static/data/examples/G213/assembly/assembled-contigs.fa', 
+                      //fastaURL: 'https://xcvf-ginkgo-example-docs.s3.us-east-2.amazonaws.com/G213/assembly/assembled-contigs.fa',
+                      indexed: false, //must include this if not including indexURL option
+      },               
       tracks: [
                   {
-                      url: 'static/data/public/test.bw',
-                      name: 'BigWig'
+                      name: 'Prefilter Alignments: BAM Format',
+                      url: '/static/data/examples/G213/alignments/G213.filtered.bam',
+                      indexed: false,
+                      format: 'bam',
+                      type: 'alignment',
+                      visibilityWindow: 300000,
+                      height: 150
                   },
-                  {
-                      url: 'static/data/public/test.bedGraph',
-                      name: 'bedgraph'
-                  }
-              ]
+              ],
+// Define global (across all tracks) "region of interest" set
+
   };
 
   browser = igv.createBrowser(div, options);

6a8fba84ebd2e090901e6f6b334d11bb001bd6c8
Sammi Smith <sammi.smith@peraton.com>
2021-12-03 15:45:10 -0500

    updated filenames, built filepaths with JS template strings, updated reference and track names to match inter-module-file-map

--- igvjs/static/js/init.js
+++ igvjs/static/js/init.js
@@ -4,29 +4,47 @@ function initBrowser() {
           options,
           browser;
 
+  //define variables to specify file locations to pass to options
+  var sample_id = 'G213';
+  var base_folder = '/static/data/examples/';
+
+  //only required for reference: fastaURL (to view a local file instead of URL)
+  var server_and_port = 'http://localhost:5000';
+
+  //reference
+  var preprocessing_genome_assembly = '/assembly/assembled-contigs.fa';
+  var fastaURL_preprocessing_genome_assembly = `${server_and_port}${base_folder}${sample_id}${preprocessing_genome_assembly}`;
+
+  //tracks
+  //all below paths are relative to base_folder/sample_id
+  var prefilter_alignments = '/alignments/engineered_align.bam';
+  var url_prefilter_alignments = `${base_folder}${sample_id}${prefilter_alignments}`;
+
+  console.log(fastaURL_preprocessing_genome_assembly);
+  
+
   div = document.getElementById("myDiv");
+  
+  //specify browser config
   options = {
       reference: {            
-                      id: 'id',
-                      name: 'name',
-                      //fastaURL: 'https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/hg19/hg19.fasta',
-                      //fastaURL must include http://localhost:5000 to view a local file instead of URL
-                      fastaURL: 'http://localhost:5000/static/data/examples/G213/assembly/assembled-contigs.fa', 
-                      //fastaURL: 'https://xcvf-ginkgo-example-docs.s3.us-east-2.amazonaws.com/G213/assembly/assembled-contigs.fa',
+                      id: sample_id,
+                      name: 'Genome assembly',
+                      fastaURL: fastaURL_preprocessing_genome_assembly, 
                       indexed: false, //must include this if not including indexURL option
       },               
       tracks: [
                   {
-                      name: 'Prefilter Alignments: BAM Format',
-                      url: '/static/data/examples/G213/alignments/G213.filtered.bam',
-                      indexed: false,
+                      name: 'Prefilter: Alignments/coverage of all positive reads to assembled genome',
+                      url: url_prefilter_alignments,
+                      indexed: false, //will be adding an index file
                       format: 'bam',
                       type: 'alignment',
                       visibilityWindow: 300000,
                       height: 150
                   },
               ],
-// Define global (across all tracks) "region of interest" set
+      // Define global (across all tracks) "region of interest" set
 
   };
 
